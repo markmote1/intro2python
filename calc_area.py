aa5b9cf97da5b0b31c46c80725bf173150526b46
eMobilis <markmote12@gmail.com>
2023-06-26 09:06:37 +0300

    area calc

---
+++ calc_area.py
@@ -0,0 +1,6 @@
+object_length = input("length of object")
+length = int(object_length)
+object_height = input("Height of object")
+height = int(object_height)
+calc_area = length*height
+print(calc_area)
