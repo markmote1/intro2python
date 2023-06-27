841df13548eecf9ceae21b2cca9e7b9966d55887
eMobilis <markmote12@gmail.com>
2023-06-27 10:15:02 +0300

    betting

---
+++ betting.py
@@ -0,0 +1,13 @@
+number = int(input("Enter your lucky number"))
+if number == 1:
+    print("10,000")
+elif number == 2:
+    print("20,000")
+elif number == 3:
+    print("5,000")
+elif number == 4:
+    print("40")
+elif number == 5:
+    print("20")
+else:
+    print("LOSE")
