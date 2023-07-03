675509318f344b28db42b745582e1f8d741c83cf
eMobilis <markmote12@gmail.com>
2023-07-03 09:44:20 +0300

    function to calculate months

---
+++ Functions/function1.py
@@ -0,0 +1,9 @@
+def sayhello():
+     print("Welcome back")
+
+sayhello()
+
+def morning():
+    print("Good morning")
+
+morning()
---
+++ Functions/functionsEx.py
@@ -0,0 +1,6 @@
+def calculate_months():
+    age = int(input("Enter age"))
+    monthslived = age * 12
+    print(f"You have lived {monthslived} months")
+
+calculate_months()
