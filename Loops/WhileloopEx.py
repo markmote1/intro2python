8a4ac8f291c471a58b47d88a8c62950a8954f592
eMobilis <markmote12@gmail.com>
2023-07-03 09:13:08 +0300

    whileloop

---
+++ Loops/WhileloopEx.py
@@ -0,0 +1,10 @@
+#using range 1-1000
+# #print all the numbers using while loop
+#ensure user inputs number in the terminal
+i = int(input("Enter number"))
+n = int(input("Enter second number"))
+
+
+while i <= n:
+    print(i)
+    i = i + 1
