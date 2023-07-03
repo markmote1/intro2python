4020262e8b0ad39e9d8b8bf7eb0f5ffffbc4b2c5
eMobilis <markmote12@gmail.com>
2023-07-03 10:28:09 +0300

    functions with param

---
+++ Functions/functionBMI.py
@@ -0,0 +1,6 @@
+def calculate_bmi():
+    weight = float(input("Enter weight in Kgs"))
+    height = float(input("Enter height metres"))
+    bmi = weight / (height * height)
+    print(f"Your BMI is {bmi}")
+calculate_bmi()
---
+++ Functions/functionswithparams.py
@@ -0,0 +1,13 @@
+def addnums(x, y):
+    result = x + y
+    print(result)
+
+
+addnums(10, 15)
+
+
+
+def showname(first, last):
+    print(first, last)
+
+showname(input("enter first name"), input("enter last name"))
