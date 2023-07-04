87c620ed01c2d3351024b3db324216ea33d2aaf3
eMobilis <markmote12@gmail.com>
2023-07-04 10:13:28 +0300

    house_object

---
+++ OOP/house.py
@@ -0,0 +1,6 @@
+class House:
+    def __init__(self, owner, location, make, colour):
+        self.owner = owner
+        self.location = location
+        self.make = make
+        self.colour = colour
---
+++ OOP/house_object.py
@@ -0,0 +1,11 @@
+from house import House
+
+person1 = House("Mark", "Juja", "Stone", "Turqoise")
+person2 = House("Linda", "Bethany", "Wood", "Black")
+person3 = House("Peter", "Canaan", "Mud", "Brown")
+person4 = House("Sofi", "Bethlehem", "Grass", "Grey")
+
+print(person1.owner, person1.location, person1.make, person1.colour)
+print(person2.owner, person2.location, person2.make, person2.colour)
+print(person3.owner, person3.location, person3.make, person3.colour)
+print(person4.owner, person4.location, person4.make, person4.colour)
